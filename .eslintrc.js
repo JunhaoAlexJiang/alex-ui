9e5c38627542b868dbc1bc13d13ee35ded08a8e9
Alexjiang <844670507@qq.com>
2022-07-27 21:52:32 +0800

    配置eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    // parser: '@babel/eslint-parser',
+  },
+  rules: {
+    // 关闭驼峰命名规则
+    "vue/multi-word-component-names": "off",
+  },
+};
