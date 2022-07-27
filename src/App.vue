3a360d57a5adbe75228fe1555d6e185778f66afb
Alexjiang <844670507@qq.com>
2022-07-27 22:46:47 +0800

    button组件type功能完成

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
   <div id="app">
     <div class="row">
       <al-button>按钮</al-button>
-      <al-button>按钮</al-button>
-      <al-button>按钮</al-button>
-      <al-button>按钮</al-button>
-      <al-button>按钮</al-button>
+      <al-button type="primary">按钮</al-button>
+      <al-button type="success">按钮</al-button>
+      <al-button type="info">按钮</al-button>
+      <al-button type="warning">按钮</al-button>
+      <al-button type="danger">按钮</al-button>
     </div>
   </div>
 </template>
@@ -16,4 +17,10 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.row {
+  .al-button {
+    margin-left: 10px;
+  }
+}
+</style>
--- src/components/button.vue
+++ src/components/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="al-button">
+  <button class="al-button" :class="[`al-button-${type}`]">
     <span><slot /></span>
   </button>
 </template>
@@ -7,6 +7,12 @@
 <script>
 export default {
   name: "alButton",
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+  },
 };
 </script>
 
@@ -40,6 +46,63 @@ export default {
     color: #409eff;
     border-color: #c6e2ff;
     background-color: #ecf5ff;
+  }
+}
+
+//type属性样式
+.al-button-primary {
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+  &:hover,
+  &:focus {
+    background: #66b1ff;
+    background-color: #66b1ff;
+    color: #fff;
+  }
+}
+.al-button-success {
+  color: #fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+  &:hover,
+  &:focus {
+    background: #85ce61;
+    background-color: #85ce61;
+    color: #fff;
+  }
+}
+.al-button-info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+  &:hover,
+  &:focus {
+    background: #a6a9ad;
+    background-color: #a6a9ad;
+    color: #fff;
+  }
+}
+.al-button-warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+  &:hover,
+  &:focus {
+    background: #ebb563;
+    background-color: #ebb563;
+    color: #fff;
+  }
+}
+.al-button-danger {
+  color: #fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+  &:hover,
+  &:focus {
+    background: #f78989;
+    background-color: #f78989;
+    color: #fff;
   }
 }
 </style>
